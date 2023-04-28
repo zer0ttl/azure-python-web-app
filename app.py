024dc753685550ea2ba02f978afe4fe88c3756cd
zer0ttl <35877460+zer0ttl@users.noreply.github.com>
2023-04-28 13:40:31 -0600

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/060ccdaa3c23c62dd5484f6bb58d74491c54bcb99096557fb839abb613baa163")
-def hello():
+def poc():
     return "A proof of concept for subdomain takeover."
 
 @app.route("/")
-def hello():
+def home():
     return "200 OK"
