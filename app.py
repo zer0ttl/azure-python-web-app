71361701153eafac575027245b797217ad3091ea
zer0ttl <35877460+zer0ttl@users.noreply.github.com>
2023-04-28 13:33:44 -0600

    Update app.py

--- app.py
+++ app.py
@@ -4,3 +4,7 @@ app = Flask(__name__)
 @app.route("/060ccdaa3c23c62dd5484f6bb58d74491c54bcb99096557fb839abb613baa163")
 def hello():
     return "A proof of concept for subdomain takeover."
+
+@app.route("/")
+def hello():
+    return "200 OK"
